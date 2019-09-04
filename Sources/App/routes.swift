7bad3ccd15efc1de1d6f7b3afd34b52146daea81
Lucas Fernandez Nicolau <dev.lucasfn@icloud.com>
2019-09-03 22:06:06 -0300

    Primeiros testes.

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -11,6 +11,16 @@ public func routes(_ router: Router) throws {
     router.get("hello") { req in
         return "Hello, world!"
     }
+    
+    router.get("persons") { req in
+        return """
+            Person: {
+                name: Lucas,
+                age: 21,
+                height: 1.78,
+            }
+        """
+    }
 
     // Example of configuring a controller
     let todoController = TodoController()
